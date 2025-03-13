e2a2035b4e83207eb588c387c92e20a4ab4ec79a
Mirvahid Karimov <mirvahidvk-bb104@code.edu.az>
2025-03-13 11:05:14 +0400

    Task 1 (experimenting)

--- Program.cs
+++ Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
+            #region Task 1
+            // Print hello world
+
             Console.WriteLine("Hello, World!");
+            #endregion
+
         }
     }
 }
