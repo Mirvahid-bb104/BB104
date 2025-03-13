78977c59bc32f8bf7315a54521c05c6f162afde6
mirvahid <mirvahidvk-bb104@code.edu.az>
2025-03-13 23:48:32 +0400

    (13.03.2025) Task3

--- Program.cs
+++ Program.cs
@@ -1,14 +1,21 @@
-﻿namespace BB104
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace BB104
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            //int num = 8;
-            //if (num % 2 == 0)
-            //    Console.WriteLine("num cutdur");
+            //int num1 = 8;
+            //int num2 = 9;
+            //int sum = 0;
+            //if (num1 % 2 == 0 && num2 % 2 == 0)
+            //{
+            //    sum = num1 + num2;
+            //    Console.WritLine(sum);
+            //}
             //else
-            //    Console.WriteLine("num tekdir");
+            //    Console.WriteLine("DAXIL OLUNAN EDEDLER CUT ILMALIDIR");
         }
     }
 }
