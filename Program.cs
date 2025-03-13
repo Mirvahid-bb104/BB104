5582ef8b5bbf347929573f277afe85821e2ce406
mirvahid <mirvahidvk-bb104@code.edu.az>
2025-03-14 01:46:20 +0400

    (14.04.2025) Task7

--- Program.cs
+++ Program.cs
@@ -7,16 +7,13 @@ namespace BB104
     {
         static void Main(string[] args)
         {
-    //        int a = 45;
-    //        int b = 64;
-    //        int c = 75;
-    //        int sum = 0;
-    //        if (a % 5 == 0)
-    //            sum += a;
-    //        else if (b % 5 == 0)
-    //            sum += b;
-    //    }   else if (char % 5 == 0)
-    //        else 
-    //        Console.WritLine("bese bolunen eded yoxdur")
-    //}
+            //int x = 1;
+            //int y = 2;
+            //int sum = 0;
+            //for (int i = 1; i < 10; i++)
+            //    if (sum = x + y ;x = y;y=sum)
+        }  
+          
+           
+    }
 }
